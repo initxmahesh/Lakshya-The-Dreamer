c72027dd6bdced6472adadbef76f5e3f5daf85ae
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-08-13 16:15:48 +0545

    feat: dark and light mode toggle feature is added

---
+++ src/components/CareerGoals.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const CareerGoals = () => {
+  return (
+    <>
+      <div>
+        <h1>Your Career Goals</h1>
+        <p>
+          Set and track goals that will help you achieve your desired job roles
+          and career path
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default CareerGoals;
